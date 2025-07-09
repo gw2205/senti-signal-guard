2fc66ec32a226bbe612605f0bc126463ef2c4a5f
gw2205 <gaiwinter111@gmail.com>
2025-07-09 10:36:58 +0300

    fix: clean up telegram_bot.send_message

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,10 +1,14 @@
-# Telegram Bot functions
+#!/usr/bin/env python3
 import os
 import requests
 
-def send_message(msg):
-    token = os.getenv("TELEGRAM_BOT_TOKEN")
+def send_message(msg: str) -> dict:
+    token   = os.getenv("TELEGRAM_BOT_TOKEN")
     chat_id = os.getenv("TELEGRAM_CHAT_ID")
-    url = f"https://api.telegram.org/bot{token}/sendMessage"
-    data = {"chat_id": chat_id, "text": msg}
-    return requests.post(url, data=data).json()
+    url     = f"https://api.telegram.org/bot{token}/sendMessage"
+    payload = {
+        "chat_id": chat_id,
+        "text": msg,
+        "parse_mode": "Markdown"
+    }
+    return requests.post(url, data=payload).json()
